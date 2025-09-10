2fa319a7006ddd18c0b650912e4bc0f2620a67ef
CodaBool <CodaBool@users.noreply.github.com>
2025-09-10 03:43:17 +0000

    build 132 hash c929476

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,38 @@
-// can write embed rules here for platforms like twitter
+const description = 'RPG maps with customizable content'
+const title = 'Stargazer'
 export const metadata = {
-  title: 'Stargazer',
-  description: 'RPG maps with customizable content',
+  title,
+  description,
+  metadataBase: new URL(process.env.NEXT_PUBLIC_URL),
+  openGraph: {
+    title,
+    description,
+    url: process.env.NEXT_PUBLIC_URL,
+    siteName: title,
+    images: [
+      {
+        url: '/embed.webp', // 1200x630 recommended
+        width: 1164,
+        height: 1094,
+        alt: description,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    creator: process.env.NEXT_PUBLIC_USER,
+    images: ['/embed.webp'],
+  },
+  icons: {
+    icon: '/icon.jpg',
+    shortcut: '/icon.jpg',
+    apple: '/icon.jpg',
+  },
+  themeColor: '#0d0d0d',
 }
 
 // allows for mobile devices to properly style
