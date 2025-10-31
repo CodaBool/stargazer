b28f70a8b605e69104ebdac2efb39c727714c859
CodaBool <CodaBool@users.noreply.github.com>
2025-10-31 23:45:08 +0000

    build 142 hash 81f3c76

--- app/api/release/route.js
+++ app/api/release/route.js
@@ -5,7 +5,7 @@ export async function GET(req) {
   try {
 
     const now = new Date();
-    const release = new Date('2025-10-31');
+    const release = new Date('2025-12-31');
     const diff = Math.ceil((release.getTime() - now.getTime()) / (1000 * 60 * 60 * 24));
 
     return new Response(
@@ -33,7 +33,7 @@ export async function GET(req) {
             </style>
         </head>
         <body>
-            <div>${diff} days until Stargazer release</div>
+            <div>DELAYED! new Stargazer release will be in ${diff} days</div>
         </body>
         </html>
         `,
