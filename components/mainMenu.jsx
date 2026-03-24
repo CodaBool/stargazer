047eb5df2419a005d7f8802e20228c60756c6ccf
CodaBool <CodaBool@users.noreply.github.com>
2026-03-24 20:00:42 +0000

    build 286 hash af579fd

--- components/mainMenu.jsx
+++ components/mainMenu.jsx
@@ -406,7 +406,7 @@ export function MainMenu({ cloudMaps, session, revalidate, hash, systems }) {
               }
             </div>
           ))}
-          {systems.length === 2 && <p className='mb-10 mt-2 text-center'>Looking for more maps? Checkout one of the <a href="https://github.com/CodaBool/stargazer/wiki/Forks" className='text-blue-300'>forks</a></p>}
+          {/* {systems.length === 2 && <p className='mb-10 mt-2 text-center'>Looking for more maps? Checkout one of the <a href="https://github.com/CodaBool/stargazer/wiki/Forks" className='text-blue-300'>forks</a></p>}*/}
         </div>
       )}
 
